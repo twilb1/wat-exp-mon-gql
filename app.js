979352c6b2fbb234177cf75a3eb6d1e35039026d
Len Baker <Len.Baker@TECHNICALLYWRITEIT.COM>
2020-06-07 19:36:24 +0100

    adding a Proc file for Heroku

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ app.use("/graphql", ExpressGraphQL({
     graphiql: true         // Enables the GraphQL Playground
 }));
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000...")
-}); 
+
+// Start listening to the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}.... `));
